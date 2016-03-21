83d5a1bcd89086664987d2fc57b1fb4ef777f0fc
geogie <geogie.ren@firstleap.cn>
2016-03-21 11:10:05 +0800

    init four

--- app/src/main/java/app/coolweather/com/myweathecool/activity/ChooseAreaActivity.java
+++ app/src/main/java/app/coolweather/com/myweathecool/activity/ChooseAreaActivity.java
@@ -62,6 +62,10 @@ public class ChooseAreaActivity extends Activity {
      * 当前选中的级别
      */
     private int currentLevel;
+    /**
+     *  是否从WeatherActivity 中 跳 转过来。
+     */
+    private boolean isFromWeatherActivity;
 
     public ChooseAreaActivity() {
     }
@@ -69,9 +73,11 @@ public class ChooseAreaActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
         SharedPreferences prefs = PreferenceManager.
                 getDefaultSharedPreferences(this);
-        if (prefs.getBoolean("city_selected", false)) {
+        //  已经选择了城市且不是从WeatherActivity 跳转过来，才会直接跳转到WeatherActivity
+        if (prefs.getBoolean("city_selected", false) && !isFromWeatherActivity) {
             Intent intent = new Intent(this, WeatherActivity.class);
             startActivity(intent);
             finish();
@@ -246,6 +252,10 @@ public void onBackPressed() {
     } else if (currentLevel == LEVEL_CITY) {
         queryProvinces();
     } else {
+        if (isFromWeatherActivity) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+        }
         finish();
     }
 }
